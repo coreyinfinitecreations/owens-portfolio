e91b7b76bbc87ef7cd0294793b6ebc5756bb2385
Corey Owens <152214419+coreyswordsweeper@users.noreply.github.com>
2024-07-23 19:11:06 -0500

    Updated website content

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,18 +7,8 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
+    title: 'Providence Medical Clinic',
+    description: `Redesigned the website for a non-profit organization that helps people in Northeast Tennessee that fall below the 150% poverty line to gain access to free healthcare. I provided solutions to ensure patients are getting the quality care they need before they walk into the office.`,
     imgSrc: '/static/images/time-machine.jpg',
     href: '/blog/the-time-machine',
   },
