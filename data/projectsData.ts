e81fc87432c6e205d0e217fbc868d753c20c65ec
Corey Owens <152214419+coreyswordsweeper@users.noreply.github.com>
2024-07-31 18:49:53 -0500

    Update link on project card

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -9,8 +9,7 @@ const projectsData: Project[] = [
   {
     title: 'Providence Medical Clinic',
     description: `Redesigned the website for a non-profit organization that helps people in Northeast Tennessee that fall below the 150% poverty line to gain access to free healthcare. I provided solutions to ensure patients are getting the quality care they need before they walk into the office.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    href: 'https://www.providenceclinic.org',
   },
 ]
 
